572e381ee10a0bf911460631c832e87b7a58f3c4
rollytiberius <rollytiberius@gmail.com>
2024-11-10 13:47:14 +0000

    added the functions lesson

---
+++ dataTypesAndConditionals/script.js
@@ -0,0 +1,26 @@
+function favoriteAnimal(animal) {
+  return animal + " is my favorite animal!";
+}
+
+console.log(favoriteAnimal("Goat"));
+console.log(favoriteAnimal("Curcan"));
+
+function favoriteFood(food) {
+  return `My favorite food is ${food}`;
+}
+
+console.error(favoriteFood("Burger"));
+
+function add7(number) {
+  return number + 7;
+}
+
+console.warn(add7(7));
+
+// Multiply function
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(5, 6));
