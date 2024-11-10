d435c69f1dbe2ed228b2e5a6ed354149bd2d53af
rollytiberius <rollytiberius@gmail.com>
2024-11-10 10:24:47 +0000

    Variable and operators lesson compleated

---
+++ variableAndOperators/script.js
@@ -0,0 +1,33 @@
+const mname = "Rolland";
+const surname = "Doe";
+
+console.log(mname);
+console.log(surname);
+
+let age = 11;
+console.log(age); // Outputs 11 to the console
+
+age = 34;
+
+console.log("Dana"); // What will be output now?
+
+let pi = 3.14;
+pi = 30;
+
+console.log(pi); // What will be output?
+
+console.log(3 + 2 - 76 * (1 + 1)); // What will be output?
+
+//Exercises
+
+console.log(23 + 97);
+console.log(23 + 97 + 57 + 135 + 67 + 89);
+console.log((4 + 6 + 9) / 77);
+
+const a = 60;
+
+console.log(a);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
