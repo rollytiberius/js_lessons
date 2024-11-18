6e51c7d88fb1fd02229df9e416ac3c4cf95c08ac
rollytiberius <rollytiberius@gmail.com>
2024-11-18 07:30:21 +0000

    add new lessons

---
+++ shopingList/script.js
@@ -0,0 +1,11 @@
+const ulParent = document.querySelector("body>ul");
+const input = document.querySelector("input");
+const buttoninsert = document.getElementById("inputBtn");
+
+function buttonSort() {
+  const currentValue = buttoninsert.value;
+  buttoninsert.value = "";
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const dbutton = document.createElement("button");
+}
