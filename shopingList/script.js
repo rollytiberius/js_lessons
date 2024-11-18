2d0ae440b98b8c0bc13988d9886687c9b589a459
roland_work <rollytiberius@gmail.com>
2024-11-18 13:49:01 +0000

    Finished te shoping list

--- shopingList/script.js
+++ shopingList/script.js
@@ -3,9 +3,21 @@ const input = document.querySelector("input");
 const buttoninsert = document.getElementById("inputBtn");
 
 function buttonSort() {
-  const currentValue = buttoninsert.value;
-  buttoninsert.value = "";
+  const currentValue = input.value;
   const li = document.createElement("li");
   const span = document.createElement("span");
   const dbutton = document.createElement("button");
+  span.textContent = currentValue;
+  dbutton.textContent = "Delete";
+  li.appendChild(span);
+  li.appendChild(dbutton);
+  ulParent.appendChild(li);
+  input.value = "";
+  function removeItem() {
+    li.parentNode.removeChild(li);
+  }
+
+  dbutton.addEventListener("click", removeItem);
 }
+
+buttoninsert.addEventListener("click", buttonSort);
